8ecd2d1531c0720cb486d67f972f2538fa32cf23
Noble Sunil <160707135+NobleSunil@users.noreply.github.com>
2025-09-15 14:53:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template
-import os  # Needed to read environment variables
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Hello from OpenShift!"
 
 if __name__ == '__main__':
-    # Use PORT from OpenShift if available, default to 8080
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
